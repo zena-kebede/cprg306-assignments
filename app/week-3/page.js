1494c2e79bc005a3401ec1bc7b8194027328cec2
Zena Kebede <zena.kebede@edu.sait.ca>
2024-09-19 14:49:16 -0600

    Create week-3 folder
    create page.js file, and add dog objects.

---
+++ app/week-3/page.js
@@ -0,0 +1,18 @@
+export default function Page() {
+    let dog1 = {
+        name: "Buddy",
+        breed: "Golden Retriever",
+        age: "5",
+        color: "Golden"
+    };
+    let dog2 = {
+        name: "Nini",
+        breed: "Bengal",
+        age: "4",
+        color: "striped"
+    };
+    let dog 3 = {
+        name: "Molly",
+        breed: 
+    }
+}
