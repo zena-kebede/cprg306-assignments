13c4cf19d13246df51f44afa2bfc17da50a3fef9
Zena Kebede <zena.kebede@edu.sait.ca>
2024-09-19 15:03:42 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,20 +1,26 @@
 export default function Page() {
+};
+return (
+    <main className="ml-4">
+        <h1 className="text-2xl font-bold"> Week 3 Demo </h1>
+        <section className="m-2"
     let dog1 = {
         name: "Buddy",
         breed: "Golden Retriever",
         age: 5,
         color: "Golden"
-    };
+    },
     let dog2 = {
         name: "Nini",
         breed: "Bengal",
         age: 4,
         color: "Black/White Stripes"
-    };
+    },
     let dog3 = {
         name: "Molly",
         breed: "Tabby",
         age: 16,
         color: "Orange"
-    }
-}
+    };
+    </main>
+)
