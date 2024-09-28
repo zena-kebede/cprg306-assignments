85d3ef7320d084e0b99416dccc828d84586357ae
Zena Kebede <zena.kebede@edu.sait.ca>
2024-09-27 20:03:10 -0600

    update item.js
    rearranged items to align the quantity

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Item = ({ name, quantity, category }) => {
-    return (
-        <li className="flex justify-between p-2 border-b border-gray-200">
-            <span className="font-bold">{name}</span>
-            <span>{quantity}</span>
-            <span className="text-gray-500">{category}</span>
-        </li>
-    );
+  return (
+    <li className="p-2 border-b border-gray-200 flex justify-between items-center">
+      <div className="flex-grow">
+        <span className="font-semibold">{name}</span> - ({category})
+      </div>
+      <div className="flex-shrink-0 w-12 text-center">{quantity}</div>
+    </li>
+  );
 };
 
 export default Item;
