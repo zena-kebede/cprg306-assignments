15f927b5c292831783f2bb8d495700b7180b1733
Zena Kebede <zena.kebede@edu.sait.ca>
2024-09-27 19:35:34 -0600

    Update page.js
    functional component named Page that returns main element wrapped in h1 "Shopping List" and the ItemList component. Tailwind included.

---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemList from './item-list';
+
+const Page = () => {
+    return (
+        <main className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
+            <ItemList/>
+        </main>
+    );
+};
+
+export default Page;
