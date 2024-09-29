7a785ebca088920505fd9e2ef93aa407e22178b6
Zena Kebede <zena.kebede@edu.sait.ca>
2024-09-28 23:08:54 -0600

    Update if statements in new-item.js
    If statements added to increment and decrement setQuantity

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -5,16 +5,29 @@ import { useState } from "react";
 export default function Counter() {
     const [quantity, setQuantity] = useState(1);
 
-    const increment = () => setQuantity(quantity + 1);
-    const decrement = () => setQuantity(quantity - 1);
+    const increment = () => {
+        if (quantity < 20) {
+            setQuantity(quantity + 1)
+        }
+    };
+
+    const decrement = () => {
+        if (quantity > 1) {
+            setQuantity(setQuantity - 1);
+        }
+    };
 
     return (
         <div>
             <h1>Counter component</h1>
             <p>Count: {quantity}</p>
-            <button onClick={increment} className="bg-white">+</button>
-            <button onClick={decrement} className="bg-blue-500">-</button>
+            <button 
+            onClick={increment} 
+            className="bg-white">+</button>
+            <button 
+            onClick={decrement} 
+            className="bg-blue-500">-</button>
         </div>
     );
-};
+}
 
