160fde417466f7ead5f6174803225bb55d8fb260
Zena Kebede <zena.kebede@edu.sait.ca>
2024-10-14 20:11:07 -0600

    copied 2 pages from week 4, updated main page.js
    Copied page.js and new-item.js from week-4 to week 5 and updated the main page to include week-5.

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function StudentInfo() {
       <Link href="http://localhost:3000/week-3"> Week 3</Link>
       <h1>CPRG 306: Web Development 2 - Assignments </h1>
       <Link href="http://localhost:3000/week-4"> Week 4</Link>
+      <h1>CPRG 306: Web Development 2 - Assignments </h1>
+      <Link href="http://localhost:3000/week-5"> Week 5</Link>
     </div>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+import Counter from "./counter.js";
+import Quantity from "./new-item.js";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>Week 4 - Basic Interactivity</h1>
+            <Quantity />
+        </main>
+    );
+}
