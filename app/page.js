5877ccedfb9dea15660c8f4fee78c2554e513559
Zena Kebede <zena.kebede@edu.sait.ca>
2024-11-02 20:05:17 -0600

    Update page.js
    Week-8 folder added.

--- app/page.js
+++ app/page.js
@@ -15,6 +15,8 @@ export default function StudentInfo() {
       <Link href="http://localhost:3000/week-6"> Week 6</Link>
       <h1>CPRG 306: Web Development 2 - Assignments </h1>
       <Link href="http://localhost:3000/week-7"> Week 7</Link>
+      <h1>CPRG 306: Web Development 2 - Assignments </h1>
+      <Link href="http://localhost:3000/week-8"> Week 8</Link>
     </div>
   );
 }
